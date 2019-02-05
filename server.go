18b910ae6303c77a8c6f53beb158a87c5b091b47
Ansel Bridgewater <iamansel@gmail.com>
2019-02-05 13:59:33 -0800

    Refactored the code to return a job in a given location

--- server.go
+++ server.go
@@ -1,43 +1,52 @@
 package main
 
 import (
-    "net/http"
-    "encoding/json"
-    "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
-    "github.com/labstack/echo"
-    "github.com/labstack/echo/middleware"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
+// Job : The job struct holds what we think is the most important data.
 type Job struct {
-    JobType string `json:"type"`
-    Company string `json:"company"`
-    Title string `json:"title"`
-    Location string `json:"location"`
-    Description string `json:"description"`
+	JobType     string `json:"type"`
+	Company     string `json:"company"`
+	Title       string `json:"title"`
+	Location    string `json:"location"`
+	Description string `json:"description"`
+}
+
+// getJob : The getJob function returns the first job posting as HTML in a string based on the location passed in.
+func getJob(location string) string {
+	// Calling GitHub jobs API
+	response, _ := http.Get("https://jobs.github.com/positions.json?description=python&full_time=true&location=" + location)
+	// Get body of the response
+	data, _ := ioutil.ReadAll(response.Body)
+	// Instantiate a new job struct
+	var job []Job
+	// Unmarshal data into a pointer to the job struct
+	json.Unmarshal(data, &job)
+	// Create HTML string
+	jobPosting := "Company: <br>" + job[0].Company + "<br><br> Description: " + job[0].Description
+
+	return jobPosting
 }
 
 func main() {
-    // Echo instance
+	// Echo instance
 	e := echo.New()
 
 	// Logger middleware
 	e.Use(middleware.Logger())
 
-    // Calling GitHub jobs API
-    response, _ := http.Get("https://jobs.github.com/positions.json?description=python&full_time=true&location=sf")
-    // Get body of the response
-    data, _ := ioutil.ReadAll(response.Body)
-    // Instantiate a new job struct
-    var job []Job
-    // Unmarshal data into a pointer to the job struct
-    json.Unmarshal(data, &job)
-    // Create HTML string
-    jobPosting := "Company: <br>" + job[0].Company + "<br><br> Description: " + job[0].Description
-
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, jobPosting)
+		// location get the query parameter, so you must add /?location=sf to the end
+		location := c.QueryParam("location")
+		jobPost := getJob(location)
+		return c.HTML(http.StatusOK, jobPost)
 	})
 
 	// Start server
